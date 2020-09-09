54a5fba18e58932a0d911937ce9ace19ce9c88e1
Yovani Cruz <cruz_yovani@yahoo.com>
2020-09-09 17:44:51 -0500

    connected files

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
+const inquirer = require("inquirer");
+const fs = require("fs"); 
+const path = require("path")
+
+
+const generateMarkdown = require("./utils/generateMarkdown")
+
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+      },
+      {
+        type: "input?",
+        message: "What is your GitHub user name?",
+        name: "GitHub",
+    
+      },
+      {
+        type: "list",
+        message: "What is your preferred method of communication?",
+        name: "contact",
+        choices: [
+          "email",
+          "phone",
+          "telekinesis"
+        ]
+      }   
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 // function to initialize program
 function init() {
+inquirer.prompt(questions).then(function(answers){
+    console.log (answers)
 
+    writeToFile("README.md", generateMarkdown(answers))
+})
 }
 
 // function call to initialize program
